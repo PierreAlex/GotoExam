68767fe25e5060daee26e37d27268bca5bc28f27
Pierre-Alexandre <pierrealexleblanc@gmail.com>
2022-11-18 09:38:29 -0500

    Removing error handling because it is not done properly anyway and there wont be any error in this demo.

--- src/utils/computeStockListRecommendation.ts
+++ src/utils/computeStockListRecommendation.ts
@@ -3,22 +3,6 @@ import { ComputedStockInfo, ComputedStocksList } from '../types/stockInfo/Comput
 
 export const computeStockListRecommendation = (stocksList: StocksDtoList): ComputedStocksList => {
     const computedStocksList = stocksList.stocksList.map((stockInfo): ComputedStockInfo => {
-        if (stockInfo.stockPriceList.length == 0 || stockInfo.socialMediaCountList.length == 0) {
-            return {
-                averageDeviation: 0,
-                averagePrice: 0,
-                currentPrice: 0,
-                diffPrice: 0,
-                highestPrice: 0,
-                iconUrl: '',
-                isUnstable: false,
-                lowestPrice: 0,
-                recommendation: 0,
-                socialMediaCountList: [],
-                stockCode: 'Invalid',
-                stockPriceList: [],
-            };
-        }
         const priceList = stockInfo.stockPriceList;
         const averagePrice = priceList.reduce((a, b) => a + b, 0) / priceList.length;
         const highestPrice = Math.max(...priceList);
